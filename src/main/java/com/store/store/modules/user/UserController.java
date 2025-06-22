7c4201367b3d0790daa1171c7d9283cc02f7f60c
hoangb0410 <viethoangb0410@gmail.com>
2025-06-22 23:57:44 +0700

    feat: add Security annotation to check authentication and authorization

--- src/main/java/com/store/store/modules/user/UserController.java
+++ src/main/java/com/store/store/modules/user/UserController.java
@@ -3,12 +3,17 @@ package com.store.store.modules.user;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.store.store.common.ApiResponse;
+import com.store.store.common.ErrorHelper;
 import com.store.store.model.User;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @RestController
 @RequestMapping("/users")
@@ -19,9 +24,15 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Get all users", description = "API for admin to fetch all registered users", security = @SecurityRequirement(name = "bearerAuth"))
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
-    public ResponseEntity<ApiResponse<List<User>>> getUsers() {
-        List<User> users = userService.getAllUsers();
-        return ResponseEntity.ok(ApiResponse.success(users, 200));
+    public ResponseEntity<ApiResponse<Object>> getUsers() {
+        try {
+            List<User> users = userService.getAllUsers();
+            return ResponseEntity.ok(ApiResponse.success(users, 200));
+        } catch (Exception e) {
+            return ErrorHelper.badRequest("Login failed: " + e.getMessage());
+        }
     }
 }
